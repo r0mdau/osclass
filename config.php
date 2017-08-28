4e3bf023155409807e24bb1d428c268c17821f6f
Romain Dauby <romain.dauby@orange.com>
2017-08-28 14:43:53 +0200

    Init

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * The base MySQL settings of Osclass
+ */
+define('MULTISITE', 0);
+
+/** MySQL database name for Osclass */
+define('DB_NAME', 'osclass');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'test');
+
+/** MySQL hostname */
+define('DB_HOST', 'database-osclass');
+
+/** Database Table prefix */
+define('DB_TABLE_PREFIX', 'oc_');
+
+define('REL_WEB_URL', '/');
+
+define('WEB_PATH', 'http://osclass.dev/');
+
+?>
